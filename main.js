ce37d398f142ec6f2241afdbe78ec8f7b4b26986
John Susi <john@susi.se>
2015-07-23 10:57:03 +0200

    Add better diagnostics and normalize paths

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ var bin = path.join(__dirname, 'bin', process.platform);
 var exe = path.join(bin, (process.platform == 'win32' ? '7za.exe' : '7z') );
 
 function unpack(source_file, target_dir) {
-  var result = child_process.execFileSync(
-    exe, [
-    'x', '-o' + target_dir, source_file
-  ]);
-  /*, {}, function (error, stdout, stderr) {
-
-    console.log('unpack');
-    if (error)  console.log(error);
-    if (stdout) console.log(stdout.toString('utf-8'));
-    if (stderr) console.log(stderr.toString('utf-8'));
-  });
-  */
-  console.log(result.toString('utf-8'));
+  try {
+    source_file = path.normalize(source_file);
+    target_dir  = path.normalize(target_dir);
+    console.log('unpack ' + source_file + ' to ' + target_dir);
+    var result = child_process.execFileSync(exe, [
+        'x', '-o' + target_dir, source_file]);
+    console.log(result.toString('utf-8'));
+  }
+  catch() {
+    console.error(er.stack)
+    if (er.pid) console.log('%s (pid: %d) exited with status %d',
+        er.file, er.pid, er.status)
+  }
 }
 module.exports = unpack;
